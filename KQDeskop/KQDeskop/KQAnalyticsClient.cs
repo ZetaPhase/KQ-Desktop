b90c2593451aeaeff8084cd219a409552faa8039
0xFireball <0xFireball@outlook.com>
2016-05-21 14:56:49 -0700

    add kq code

---
+++ KQDeskop/KQDeskop/KQAnalyticsClient.cs
@@ -0,0 +1,16 @@
+﻿/*
+ */
+using System;
+
+namespace KQDeskop
+{
+	/// <summary>
+	/// A class to send data to KQ Analytics
+	/// </summary>
+	public class KQAnalyticsClient
+	{
+		public KQAnalyticsClient()
+		{
+		}
+	}
+}
